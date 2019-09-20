7700fc69997afb97e3644aa6fa577da60a4a615e
IanC <ian.marc.c@gmail.com>
2019-09-20 16:55:28 -0700

    Updates

---
+++ precache-manifest.b23d4aa5915affdf475322cf2ab022a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae292466c6f97f9249c5018afefb14ab",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e24a48701a386003f224",
+    "url": "/my-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5ffe8627f4257e4395a0",
+    "url": "/my-app/static/js/2.376f8d6f.chunk.js"
+  },
+  {
+    "revision": "e24a48701a386003f224",
+    "url": "/my-app/static/js/main.c32745e7.chunk.js"
+  },
+  {
+    "revision": "9c47355d93f1cffa1d27",
+    "url": "/my-app/static/js/runtime~main.eed5ce82.js"
+  }
+]);
